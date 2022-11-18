76136bbed312304e08aaee8a74f8edb617fe6a5f
hlngoben021 <hlngoben021@student.wethinkcode.co.za>
2022-11-18 14:59:04 +0200

    ana

--- lib/main.dart
+++ lib/main.dart
@@ -12,22 +12,45 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Column(
+
+
+          children: [
+
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage("/home/anabeal/StudioProjects/anapic/asset/ana.jpg"),
+            ),
+            Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 20,horizontal: 10),
+
+
+                child: Row(
+                  children: [
+                    Icon(Icons.phone),
+                    Text("0123456789")
+                  ],
+                )
+            ),
+            Container(
+              child: Text("WE DEV"),
+            ),
+            Card(
+              child: ListTile(leading: Icon(Icons.abc),
+                title: Text("abc"),
+              ),
+            )
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
